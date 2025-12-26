172f9f578dd6f94e62fe163ec98de6fb2ed44368
minobel <minobel2000@gmail.com>
2025-12-26 22:59:23 +0600

    Hello World Msg

---
+++ src/cfehome/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+import pathlib
+from django.shortcuts import render
+
+from visits.models import Visit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request, *args, **kwargs):
+    queryset = Visit.objects.all()
+    my_title = "Hello World!"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": queryset.count(),
+    }
+    html_template = "home.html"
+    Visit.objects.create()
+    return render(request, html_template, my_context)
