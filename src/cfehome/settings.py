64edd87c80edd391f9341f61b8553891404fba0e
minobel <minobel2000@gmail.com>
2025-12-30 23:11:50 +0600

    Updated

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1hp^0anszc&8&&+qy!y@ybeel@+7qph7gat%h7)*($b2fa-bn5'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+#DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+DEBUG = config('DJANGO_DEBUG')
 print("DEBUG =", DEBUG)
 
 
@@ -91,7 +92,19 @@ DATABASES = {
     }
 }
 
+CONN_MAX_AGE = config('CONN_MAX_AGE', cast=int, default=30)
+DATABASE_URL = config('DATABASE_URL', default=None, cast=str)
 
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=CONN_MAX_AGE,
+            conn_health_checks=True,
+        )
+    }
+    
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
