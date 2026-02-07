dc3dd3480375c01eff9d8f344248c9babd8e6ad5
minobel <minobel2000@gmail.com>
2026-02-07 14:56:56 +0600

    Updated test django db yaml file

--- src/commando/tests.py
+++ src/commando/tests.py
@@ -1,8 +1,16 @@
+import os
 from django.test import TestCase
 from django.conf import settings
+
+DATABASE_URL = os.environ.get("DATABASE_URL") or ""
 
 class DatabaseConfigTest(TestCase):
     def test_database_is_configured(self):
         self.assertIn("default", settings.DATABASES)
         self.assertIn("ENGINE", settings.DATABASES["default"])
+
+class NeonDBTestCase(TestCase):
+    def test_db_url(self):
+
+        self.assertIn("neon.tech", DATABASE_URL)
 
